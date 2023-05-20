761d19958306d88951d1e12946d44198da4d7ab2
vennila720 <vennila.ramasami07@gmail.com>
2023-05-21 00:06:54 +0400

    Delete ecommerce_app

--- ecommerce_app/lib/constants/asset_images.dart
+++
@@ -1,8 +0,0 @@
-class AssetsImages {
-  static AssetsImages instance = AssetsImages();
-
-  static const String _imagesPath = "assets/images";
-
-  final String welcomeImage = "$_imagesPath/welcome.jpg";
-  final String googleLogo = "$_imagesPath/google.png";
-}
